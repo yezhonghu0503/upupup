9f640c0d84dd0cdcee7c560892c3703641590991
yezhonghu0503 <211736726@qq.com>
2022-09-01 23:40:28 +0800

    build collection list

--- src/page/collection/collection.js
+++ src/page/collection/collection.js
@@ -1,23 +1,28 @@
 import collectionData from './collection.json'
+import './collection.css'
 
 export default function collection(){
-    let items = [];
+    let {log} = console;
+    let [items] = [[]];
+
+    //数据处理
     Object.keys(collectionData).forEach(v =>{
         let o = {};
         o[v] = collectionData[v]
-        items.push(o)
+        items.push(o[v])
     })
-    console.log(items);
-    // let listitems = items.map((item)=>
-    //     <div className=''>
-    //         <div>{item}</div>
-    //         <div>{item}</div>
-    //     </div>
+    
+    
+    let listitems = items.map((item,idx)=>
+        <div key={idx} className='collection'>
+            <img className='collection_logo' src={item.logo} alt=''></img>
+            <div>{item.name}</div>
+        </div>
         
-    // )
+    )
     
     return (
-        <div>1</div>
+        <div className='main'>{listitems}</div>
         
     )
 }
